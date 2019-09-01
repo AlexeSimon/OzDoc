cb3787ba9c9136d71f736bf277d37edc3e993daf
Simon Alexe <simon.alexe@outlook.com>
2019-09-02 01:20:30 +0200

    Changed the algorithm for creating a directory tree

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -78,8 +78,16 @@ def import_module(given_name, path):
     return module
 
 
-def build_directory_tree(src):
-    pass
+# curtesy of https://stackoverflow.com/questions/229186/os-walk-without-digging-into-directories-below
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
 
 if __name__=="__main__":
     print("Error: This script is part of the OzDoc framework and should not be ran alone. "
